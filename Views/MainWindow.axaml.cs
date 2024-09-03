273a194dd8c105ce9087162bff44b6a046559cf3
LoongArum <141336798+LoongArum@users.noreply.github.com>
2024-09-03 14:40:40 +0800

    Add project files.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DataGridScrollBugTest.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
