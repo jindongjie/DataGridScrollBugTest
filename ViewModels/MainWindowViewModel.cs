35d17fc0bbe2f5b1b7b88ad20fd190128c7ce111
LoongArum <141336798+LoongArum@users.noreply.github.com>
2024-09-04 09:22:44 +0800

    Refactor MainWindow and add DataGrid styling
    Updated App.axaml to include DataGrid style and fixed duplicate FluentTheme entry. Added DataGridScrollBugTest.Tests project to solution. Refactored MainWindowViewModel.cs: changed base class, added Items property, removed Greeting property, and introduced Item class. Replaced TextBlock with DataGrid in MainWindow.axaml.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,65 @@
-﻿namespace DataGridScrollBugTest.ViewModels
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DataGridScrollBugTest.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    [ObservableProperty]
+    private ObservableCollection<Item> _items = GenerateItems(300,400);
+
+    public static ObservableCollection<Item> GenerateItems(int minCount, int maxCount)
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        var items = new ObservableCollection<Item>();
+        var random = new Random();
+        var count = random.Next(minCount, maxCount + 1);
+
+        for (var i = 0; i < count; i++)
+        {
+            items.Add(new Item());
+        }
+        return items;
+    }
+}
+
+public class Item
+{
+    private static DateTime GenerateRandomBirthDay()
+    {
+        var random = new Random();
+        var year = random.Next(1900, 2022);
+        var month = random.Next(1, 13);
+        var day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
+        return new DateTime(year, month, day);
+    }
+
+    public string Name { get; } = GenerateRandomName();
+    public int Value { get; } = GenerateRandomValue();
+
+    public DateTime BirthDay { get; } = GenerateRandomBirthDay();
+
+    private static string GenerateRandomName()
+    {
+        var names = new List<string>
+        {
+            "John",
+            "Jane",
+            "Michael",
+            "Emily",
+            "David",
+            "Sarah"
+        };
+
+        var random = new Random();
+        var index = random.Next(names.Count);
+        return names[index];
+    }
+
+    private static int GenerateRandomValue()
+    {
+        var random = new Random();
+        return random.Next();
     }
 }
